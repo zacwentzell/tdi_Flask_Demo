47b5e4dbf7da2dc0c3020c67c2f1211dcafae96a
zacwentzell <Zachary.Wentzell@stevens.edu>
2017-06-06 17:28:07 -0400

    try/except to see what's happening on heroku

--- app.py
+++ app.py
@@ -30,75 +30,78 @@ def index():
 
 @app.route('/stocks', methods=['GET', 'POST'])
 def stocks():
-    if request.method == 'POST':
-        f = request.form
-        plot_options = ','.join([opt.replace('plt_opt_', '') for opt, _ in f.items() if 'plt_' in opt])
-        tooltip_options = ','.join([opt.replace('tt_opt_', '') for opt, _ in f.items() if 'tt_opt' in opt])
-        return redirect('/stocks?name={}&plot_options={}&tooltip_options={}'.format(f['Name'], plot_options, tooltip_options))
+    try:
+        if request.method == 'POST':
+            f = request.form
+            plot_options = ','.join([opt.replace('plt_opt_', '') for opt, _ in f.items() if 'plt_' in opt])
+            tooltip_options = ','.join([opt.replace('tt_opt_', '') for opt, _ in f.items() if 'tt_opt' in opt])
+            return redirect('/stocks?name={}&plot_options={}&tooltip_options={}'.format(f['Name'], plot_options, tooltip_options))
 
-    elif request.method == 'GET':
-        ticker, script, div = '', '', ''
-        ticker = request.args.get('name', '')
+        elif request.method == 'GET':
+            ticker, script, div = '', '', ''
+            ticker = request.args.get('name', '')
 
-        plot_options = request.args.get('plot_options')
-        tooltip_options = request.args.get('tooltip_options')
+            plot_options = request.args.get('plot_options')
+            tooltip_options = request.args.get('tooltip_options')
 
-        if not plot_options:
-            plot_options = ['Close']
-        else:
-            plot_options = plot_options.split(',')
-            
-        if not tooltip_options:
-            tooltip_options = []
-        else:
-            tooltip_options = tooltip_options.split(',')
+            if not plot_options:
+                plot_options = ['Close']
+            else:
+                plot_options = plot_options.split(',')
 
-        if ticker:
-            colors = d3['Category10'][max(len(plot_options), 3)]
-            ticker = ticker.upper()
-            quandl_url = 'https://www.quandl.com/api/v3/datasets/WIKI/{}.json?api_key=8W4wYTMuHYfsy4WTyw7L'.format(ticker)
+            if not tooltip_options:
+                tooltip_options = []
+            else:
+                tooltip_options = tooltip_options.split(',')
 
-            # try:
-            quandl = requests.get(quandl_url).json()
-            data = quandl['dataset']
-            column_names = [cn.replace('-', '').replace(' ', '').replace('.', '') for cn in data['column_names']]
+            if ticker:
+                colors = d3['Category10'][max(len(plot_options), 3)]
+                ticker = ticker.upper()
+                quandl_url = 'https://www.quandl.com/api/v3/datasets/WIKI/{}.json?api_key=8W4wYTMuHYfsy4WTyw7L'.format(ticker)
 
-            df = TruthyDataFrame(data['data'], columns=column_names)
-            df['Date'] = pd.to_datetime(df['Date'])
-            df['date_str'] = df.Date.apply(str)
-            df.__bool__ = lambda x: False
-            fix_names = [name.replace('-', '').replace(' ', '').replace('.', '') for name in plot_options+tooltip_options+['Date']]
-            df = TruthyDataFrame(df.drop([col for col in df.columns if col not in fix_names], axis=1))
-            del quandl, data
+                # try:
+                quandl = requests.get(quandl_url).json()
+                data = quandl['dataset']
+                column_names = [cn.replace('-', '').replace(' ', '').replace('.', '') for cn in data['column_names']]
 
-            tooltips = []
-            for index, opt in enumerate(tooltip_options):
-                opt_fix = opt.replace('-', '').replace(' ', '').replace('.', '')
-                tooltips.append((opt, '@'+opt_fix+'{0,0.000}'))
+                df = TruthyDataFrame(data['data'], columns=column_names)
+                df['Date'] = pd.to_datetime(df['Date'])
+                df['date_str'] = df.Date.apply(str)
+                df.__bool__ = lambda x: False
+                fix_names = [name.replace('-', '').replace(' ', '').replace('.', '') for name in plot_options+tooltip_options+['Date']]
+                df = TruthyDataFrame(df.drop([col for col in df.columns if col not in fix_names], axis=1))
+                del quandl, data
+
+                tooltips = []
+                for index, opt in enumerate(tooltip_options):
+                    opt_fix = opt.replace('-', '').replace(' ', '').replace('.', '')
+                    tooltips.append((opt, '@'+opt_fix+'{0,0.000}'))
 
 
-            print(tooltips)
-            hover = HoverTool(tooltips=tooltips)
+                print(tooltips)
+                hover = HoverTool(tooltips=tooltips)
 
-            plot = figure(plot_height=200, x_axis_type='datetime',
-                          tools=['pan', 'wheel_zoom', 'box_zoom', 'reset', hover],
-                          x_axis_label='Date and Time',
-                          title='{} Historical Market Data'.format(ticker),
-                          responsive=True)
-            plot.title.align = 'center'
-            plot.title.text_font_size = '25px'
+                plot = figure(plot_height=200, x_axis_type='datetime',
+                              tools=['pan', 'wheel_zoom', 'box_zoom', 'reset', hover],
+                              x_axis_label='Date and Time',
+                              title='{} Historical Market Data'.format(ticker),
+                              responsive=True)
+                plot.title.align = 'center'
+                plot.title.text_font_size = '25px'
 
-            for index, opt in enumerate(plot_options):
-                opt_fix = opt.replace('-', '').replace(' ', '').replace('.', '')
-                plot.line('Date', opt_fix, source=df, color=colors[index], legend=opt)
+                for index, opt in enumerate(plot_options):
+                    opt_fix = opt.replace('-', '').replace(' ', '').replace('.', '')
+                    plot.line('Date', opt_fix, source=df, color=colors[index], legend=opt)
 
-            script, div = components(plot)
-            # except Exception as e:
-            #     print(str(e), ' for error')
-            #     div = '<p>ERROR: {} NOT FOUND ON QUANDL'.format(ticker)
+                script, div = components(plot)
+                # except Exception as e:
+                #     print(str(e), ' for error')
+                #     div = '<p>ERROR: {} NOT FOUND ON QUANDL'.format(ticker)
 
-    return render_template('stocks.html', ticker=ticker, script=script, div=div,
-                           plot_options=STOCK_PLOT_OPTIONS, tooltip_options=STOCK_TOOLTIP_OPTIONS)
+        return render_template('stocks.html', ticker=ticker, script=script, div=div,
+                               plot_options=STOCK_PLOT_OPTIONS, tooltip_options=STOCK_TOOLTIP_OPTIONS)
+    except Exception as e:
+        print('ERROR: ' + str(e))
 
 
 @app.route('/crypto', methods=['GET', 'POST'])
